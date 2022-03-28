a2c77280518083849e1568e680571f4d71a8709d
diferpaho <diferpaho@gmail.com>
2022-03-27 22:18:55 -0500

    changes in svg names and consult json files

--- src/components/content.tsx
+++ src/components/content.tsx
@@ -1,17 +1,27 @@
 import { useState } from "react";
 import Added from "./added";
-import Category from "./category";
-import Product from "./product";
+import products from "../json/products.json";
+import categories from "../json/categories.json";
 
 const Content = () => {
-
-    const [categories, setCategories] = useState('');
-    const [products, setProducts] = useState('');
 
     const [orders, setOrders] = useState('');
     const [selectedProduct, setSelectedProduct] = useState('');
 
-    
+    const min = {
+        height: '50px'
+    };
+
+    const minImage = {
+        height: '200px'
+    };
+
+    const addProduct = (id: number, name: string, price: number, image: string) => {
+        console.log(id)
+        console.log(name)
+        console.log(price)
+        console.log(image)
+    };
 
     return (
         <div id="main" className="container mx-auto grid grid-cols-3">
@@ -46,14 +56,20 @@ const Content = () => {
                         Deliver: Now</span>
                 </div>
 
-                <Category></Category>
-                <Category></Category>
-                <Category></Category>
-                <Category></Category>
-                <Category></Category>
-                <Category></Category>
-                <Category></Category>
-                <Category></Category>
+                {categories.map((item) => (
+                    <div key={item.id} className="col-span-2 md:col-span-1">
+                        <button className={"p-3 border shadow-lg rounded-full " + (item.id === 1 ? "bg-yellow-400" : "hover:bg-yellow-400")}>
+                            <p className="text-3xl">
+                                <img src={"assets/icons/" + item.name + ".svg"} width="40px" height="40px"
+                                    className="border rounded-full p-2 bg-white" />
+                            </p>
+                            <p className="mt-3 mb-5 text-xs">
+                                {item.name}
+                            </p>
+                        </button>
+                    </div>
+                ))}
+
 
                 <div className="col-span-1 pr-16 my-auto">
                     <button className="p-3 border shadow-lg rounded-full hover:bg-yellow-400 ">
@@ -63,10 +79,34 @@ const Content = () => {
                     </button>
                 </div>
 
-                <Product></Product>
-                <Product></Product>
-                <Product></Product>
-                <Product></Product>
+                {products.map((item) => (
+                    <div key={item.id} className="col-span-9 md:col-span-3 mx-3 mt-10">
+                        <div className="max-w-xs rounded-2xl overflow-hidden shadow-lg my-2">
+                            <img className="w-full" src={item.image} style={minImage} alt="Sunset in the mountains" />
+                            <div className="px-6 py-4">
+                                <p className="text-xs">{item.time}</p>
+                                <div className="font-bold text-sm mb-2" style={min}>{item.name}</div>
+
+                                <p className="text-grey-darker text-xs inline-flex">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                        className="bi bi-star" viewBox="0 0 16 16">
+                                        <path
+                                            d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z" />
+                                    </svg>
+                                    <span className="mx-3">{item.qualification}</span>
+                                    <span className="text-sm ml-8">${item.price}</span>
+
+                                </p>
+                                <p className=" text-center mt-3"><button onClick={() => addProduct(item.id, item.name, item.price, item.image)}
+                                    className="text-sm ml-5 bg-yellow-400 text-white mx-auto  p-1 px-6 rounded font-bold overflow-hidden">Add</button>
+
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+
+
             </div>
             <div className="2 col-span-3 lg:col-span-1  border-l">
                 <h1 className=" text-dark px-3 py-2 rounded-md text-2xl font-medium mx-16">My order</h1>
